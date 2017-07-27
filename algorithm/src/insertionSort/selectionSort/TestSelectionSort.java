3b5ffe1d50c62c458607f2013b4a5eb54d46f92d
长青 <1414728170@qq.com>
2017-07-27 08:49:19 +0800

    [*]选择排序第一版

---
+++ algorithm/src/insertionSort/selectionSort/TestSelectionSort.java
@@ -0,0 +1,33 @@
+package insertionSort.selectionSort;
+
+import sun.misc.Sort;
+
+/**
+ * Created by 长青 on 2017/7/27.
+ */
+public class TestSelectionSort {
+    public static void main(String[] args) {
+        int[] toSort = new int[] { 3, 7, 8, 6, 4, 9, 11, 25, 17 };
+        sort(toSort);
+        for (int e : toSort) {
+            System.out.println(e);
+        }
+    }
+
+    private static void sort(int[] toSort) {
+        for(int i = 0 ;i<toSort.length;i++){
+            int min = toSort[i];
+            int index = i+1;
+            for(int j=i+1;j<toSort.length;j++){
+                if(toSort[j]<min){
+                    min = toSort[j];
+                    index = j;
+                }
+            }
+            toSort[index] = toSort[i];
+            toSort[i] =min;
+        }
+    }
+
+
+}
